f693379ecad22dfb74e2a414acdeb6c89cd10099
Takahiro <deaman6948@gmail.com>
2023-08-24 23:27:14 +0900

    １−3 画面イメージ完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'users/new'
   root 'top_page#top'
+
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   devise_for :admins, controllers: {
     sessions:      'admins/sessions',
@@ -12,6 +13,12 @@ devise_for :users, controllers: {
     passwords:     'users/passwords',
     registrations: 'users/registrations'
   }
-  resources :users
+
+  devise_scope :user do
+    get 'users/confirmation/new', to: 'users/confirmations#new', as: :new_user_confirmation
+  end
+
+  resources :users 
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
