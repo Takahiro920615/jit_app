5331d49f62d257f6517cde928e50b8d7317334ab
Takahiro <deaman6948@gmail.com>
2023-09-07 22:59:00 +0900

    Active Record日本語化完了

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module JitApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
-    
+    config.autoload_paths += Dir["#{config.root}/config/locales/**/"]    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
