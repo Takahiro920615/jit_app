b545c5cb00517ba390046cdc279d199c8901d589
Takahiro <deaman6948@gmail.com>
2023-09-28 05:13:57 +0900

    article titleとcontent登録機能完了

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,26 @@
 class ArticlesController < ApplicationController
 
-def new
-  @article = Article.new
-end
+  def new
+    @article = Article.new
+  end
 
-def create
-  @article = Article.new(title: params[:article][:title], content: params[:article][:content])
-  #article.saveがfalseになっている。
-    @article.save 
-    redirect_to user_articles_url
-end
+  def create
+    user = User.find(params[:user_id])
+    @article = user.articles.build(article_params)
+    #article.saveがfalseになっている。
+      @article.save 
+      redirect_to user_articles_url(user)
+  end
 
-def index
-  @articles = Article.all
-end
+  def index
+    @articles = Article.all
+  end
 
-def show
-end
+  def show
+  end
   
-
-  
-
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 
 end
