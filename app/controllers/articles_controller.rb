63eb66ad16e95e8551049a10a8a2105bcea4a5cc
Takahiro <deaman6948@gmail.com>
2023-10-01 10:19:40 +0900

    edit update機能完成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,7 +23,31 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:user_id])
+    @article = user.articles.find(params[:id])
+
+    if params[:article][:image].present?
+      @article.image.attach(params[:article][:image])
+      
+    else
+      @article.image = 'article_images/images.png'
+    end
+
+    if @article.save 
+      redirect_to user_articles_url(user)
+    else render 'edit'
+    end
+  end
+
   
   def article_params
     params.require(:article).permit(:title, :content, :image)
